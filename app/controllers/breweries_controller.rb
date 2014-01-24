5efa48dcd6d30b6a903970601b861ced37598308
webpal2014 <trantane@mappi.helsinki.fi>
2014-01-24 19:44:21 +0200

    2_15_ok

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,5 +1,6 @@
 class BreweriesController < ApplicationController
   before_action :set_brewery, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate, only: [:destroy]
 
   # GET /breweries
   # GET /breweries.json
@@ -71,4 +72,14 @@ class BreweriesController < ApplicationController
     def brewery_params
       params.require(:brewery).permit(:name, :year)
     end
+
+    def authenticate
+	admin_accounts = { "admin" => "secret", "pekka" => "beer", "arto" => "foobar", "matti" => "ittam"}
+
+   authenticate_or_request_with_http_basic do |username, password|
+
+      username == "admin" and password == "secret"
+
+    end
+  end	
 end
